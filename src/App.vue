2656efc927de048ecd51bbcc37f18747c2aa7a88
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-05-11 15:04:58 +0800

    feat: 增加Iconify图标集

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-	<router-view />
+	<!-- <router-view /> -->
+	<IconAntDesignCodeFilled />
 </template>
 
 <style lang="scss" scoped></style>
--- vite/auto-import.ts
+++ vite/auto-import.ts
@@ -1,5 +1,7 @@
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default function createAutoImport() {
 	return [
@@ -10,19 +12,35 @@ export default function createAutoImport() {
 			resolvers: [
 				// 导入 scss
 				ElementPlusResolver({
-					importStyle: 'sass',
-				}),
-			],
+					importStyle: 'sass'
+				})
+			]
 		}),
 		// 自动导入组件
 		Components({
+			include: [/\.vue$/], // 指定加载文件
 			dts: 'src/auto-components.d.ts',
 			resolvers: [
 				// 导入 scss
 				ElementPlusResolver({
-					importStyle: 'sass',
+					importStyle: 'sass'
 				}),
-			],
+				/**
+				 * 自动引入图标组件解析器(解决手动引入)
+				 * prefix - 前缀，默认为 i，上面我们配置成了 icon，即组件名以 icon 开头
+				 * collection - 图标集名
+				 * icon - 图标名
+				 * {prefix}-{collection}-{icon}
+				 */
+				IconsResolver({
+					prefix: 'icon'
+				})
+			]
 		}),
+		// Iconify图标按需导入(需自己手动引入)
+		Icons({
+			compiler: 'vue3', // 指定编译器
+			autoInstall: true // 自动安装图标
+		})
 	];
 }
