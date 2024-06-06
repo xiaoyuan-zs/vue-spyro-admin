3aa4119bfe085fb3fcf788e2d4cd25b68dcefbf1
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-06-06 17:55:56 +0800

    fix: 布局样式调整

--- src/layouts/components/AppSetting/LayoutDrawer.vue
+++ src/layouts/components/AppSetting/LayoutDrawer.vue
@@ -8,7 +8,7 @@
 	const tabsStore = useTabsStore();
 
 	const { switchDark, setThemeColor, setDarkMenu, setWeakNessMode, setGrayMode } = useTheme();
-	const { predefineThemeColors, layoutModeOptions, animateModeOptions, tabsStyleOptions, colorModeOptions } = useLayout();
+	const { predefineThemeColors, animateModeOptions, tabsStyleOptions, colorModeOptions } = useLayout();
 
 	const menuVisible = computed(() => !['horizontal', 'basic', 'mixins'].includes(layoutStore.layout!));
 
@@ -35,106 +35,104 @@
 
 <template>
 	<el-scrollbar>
-		<el-divider>{{ $t('setting.layoutSetting') }}</el-divider>
-		<!-- 布局 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.layout') }}</span>
-			<el-select v-model="layoutStore.layout" class="w-full">
-				<el-option v-for="item in layoutModeOptions" :key="item.value" :label="item.label" :value="item.value" />
-			</el-select>
-			<LayoutSelect />
-		</div>
-		<el-divider>{{ $t('setting.themeSetting') }}</el-divider>
-		<!-- 主题模式 -->
-		<div class="drawer-item">
-			<div class="flex-center w-full">
-				<div class="flex bg-[var(--el-fill-color)] px-1 py-1 b-rd-2">
-					<div
-						v-for="item in colorModeOptions"
-						:key="item.value"
-						:class="[
-							'px-4.5 py-2 b-rd-2 flex-center cursor-pointer transition duration-200',
-							{ 'bg-[var(--el-color-info-light-7)]': item.value === layoutStore.colorMode }
-						]"
-						@click="changeColorModel(item.value)">
-						<Icon :name="item.icon" :size="20" />
+		<div class="p-5">
+			<el-divider>{{ $t('setting.layoutSetting') }}</el-divider>
+			<!-- 布局 -->
+			<div class="drawer-item">
+				<LayoutSelect />
+			</div>
+			<el-divider>{{ $t('setting.themeSetting') }}</el-divider>
+			<!-- 主题模式 -->
+			<div class="drawer-item">
+				<div class="flex-center w-full">
+					<div class="flex bg-[var(--el-fill-color)] px-1 py-1 b-rd-2">
+						<div
+							v-for="item in colorModeOptions"
+							:key="item.value"
+							:class="[
+								'px-4.5 py-2 b-rd-2 flex-center cursor-pointer transition duration-200',
+								{ 'bg-[var(--el-color-info-light-7)]': item.value === layoutStore.colorMode }
+							]"
+							@click="changeColorModel(item.value)">
+							<Icon :name="item.icon" :size="20" />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<!-- 系统主题 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.themeColor') }}</span>
-			<el-color-picker v-model="layoutStore.themeColor" color-format="hex" :predefine="predefineThemeColors" @change="changeThemeColor" />
-		</div>
-		<!-- 深色侧边栏 -->
-		<Transition name="fade-slide" mode="out-in">
-			<div class="drawer-item" v-show="unref(menuVisible)">
-				<span>{{ $t('setting.darkMenu') }}</span>
-				<el-switch v-model="layoutStore.darkMenu" inline-prompt @change="setDarkMenu" />
+			<!-- 系统主题 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.themeColor') }}</span>
+				<el-color-picker v-model="layoutStore.themeColor" color-format="hex" :predefine="predefineThemeColors" @change="changeThemeColor" />
 			</div>
-		</Transition>
-		<!-- 灰色模式 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.grayMode') }}</span>
-			<el-switch v-model="layoutStore.grayMode" inline-prompt @change="changeGrayMode" />
-		</div>
-		<!-- 色弱模式 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.weaknessMode') }}</span>
-			<el-switch v-model="layoutStore.weakness" inline-prompt @change="changeWeakness" />
-		</div>
-		<el-divider>{{ $t('setting.interfaceSetting') }}</el-divider>
-		<!-- 菜单手风琴模式 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.menuUnique') }}</span>
-			<el-switch v-model="layoutStore.menuUnique" inline-prompt />
-		</div>
-		<!-- 标签页 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.tabs') }}</span>
-			<el-switch v-model="layoutStore.tabsHidden" inline-prompt />
-		</div>
-		<!-- 标签页风格 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.tabsStyle') }}</span>
-			<el-select v-model="layoutStore.tabStyle" class="w-full">
-				<el-option v-for="item in tabsStyleOptions" :key="item.value" :label="item.label" :value="item.value" />
-			</el-select>
-		</div>
-		<!-- 标签页图标 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.tabsIcon') }}</span>
-			<el-switch v-model="layoutStore.tabsIcon" inline-prompt />
-		</div>
-		<!-- 标签页持久化 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.tabsCache') }}</span>
-			<el-switch v-model="layoutStore.tabsCache" inline-prompt @change="changeTabsCache" />
-		</div>
-		<!-- 面包屑 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.breadCrumb') }}</span>
-			<el-switch v-model="layoutStore.breadcrumbs" inline-prompt />
-		</div>
-		<!-- 面包屑图标 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.breadCrumbIcon') }}</span>
-			<el-switch v-model="layoutStore.breadcrumbsIcon" inline-prompt />
-		</div>
-		<!-- 动画类型 -->
-		<div class="drawer-item">
-			<span>{{ $t('setting.animateMode') }}</span>
-			<el-select v-model="layoutStore.animateMode" class="w-full">
-				<el-option v-for="item in animateModeOptions" :key="item.value" :label="item.label" :value="item.value" />
-			</el-select>
-		</div>
-		<el-divider>{{ $t('setting.tips') }}</el-divider>
-		<el-alert :title="$t('setting.tipDesc')" type="warning" :closable="false" />
-		<!-- 清除缓存 -->
-		<div class="mt-2 p-2 cursor-pointer bg-[var(--el-color-error-light-9)] b-rounded flex-center" @click="clearCache">
-			<Icon name="ep:delete" :size="16" color="var(--el-color-error)" />
-			<span class="text-error text-3 p-l-2">清除缓存</span>
+			<!-- 深色侧边栏 -->
+			<Transition name="fade-slide" mode="out-in">
+				<div class="drawer-item" v-show="unref(menuVisible)">
+					<span>{{ $t('setting.darkMenu') }}</span>
+					<el-switch v-model="layoutStore.darkMenu" inline-prompt @change="setDarkMenu" />
+				</div>
+			</Transition>
+			<!-- 灰色模式 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.grayMode') }}</span>
+				<el-switch v-model="layoutStore.grayMode" inline-prompt @change="changeGrayMode" />
+			</div>
+			<!-- 色弱模式 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.weaknessMode') }}</span>
+				<el-switch v-model="layoutStore.weakness" inline-prompt @change="changeWeakness" />
+			</div>
+			<el-divider>{{ $t('setting.interfaceSetting') }}</el-divider>
+			<!-- 菜单手风琴模式 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.menuUnique') }}</span>
+				<el-switch v-model="layoutStore.menuUnique" inline-prompt />
+			</div>
+			<!-- 标签页 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.tabs') }}</span>
+				<el-switch v-model="layoutStore.tabsHidden" inline-prompt />
+			</div>
+			<!-- 标签页风格 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.tabsStyle') }}</span>
+				<el-select v-model="layoutStore.tabStyle" class="w-full">
+					<el-option v-for="item in tabsStyleOptions" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+			</div>
+			<!-- 标签页图标 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.tabsIcon') }}</span>
+				<el-switch v-model="layoutStore.tabsIcon" inline-prompt />
+			</div>
+			<!-- 标签页持久化 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.tabsCache') }}</span>
+				<el-switch v-model="layoutStore.tabsCache" inline-prompt @change="changeTabsCache" />
+			</div>
+			<!-- 面包屑 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.breadCrumb') }}</span>
+				<el-switch v-model="layoutStore.breadcrumbs" inline-prompt />
+			</div>
+			<!-- 面包屑图标 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.breadCrumbIcon') }}</span>
+				<el-switch v-model="layoutStore.breadcrumbsIcon" inline-prompt />
+			</div>
+			<!-- 动画类型 -->
+			<div class="drawer-item">
+				<span>{{ $t('setting.animateMode') }}</span>
+				<el-select v-model="layoutStore.animateMode" class="w-full">
+					<el-option v-for="item in animateModeOptions" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+			</div>
+			<el-divider>{{ $t('setting.tips') }}</el-divider>
+			<el-alert :title="$t('setting.tipDesc')" type="warning" :closable="false" />
+			<!-- 清除缓存 -->
+			<div class="mt-2 p-2 cursor-pointer bg-[var(--el-color-error-light-9)] b-rounded flex-center" @click="clearCache">
+				<Icon name="ep:delete" :size="16" color="var(--el-color-error)" />
+				<span class="text-error text-3 p-l-2">清除缓存</span>
+			</div>
 		</div>
 	</el-scrollbar>
 </template>
@@ -145,7 +143,6 @@
 		align-items: center;
 		justify-content: space-between;
 		padding: 5px 0;
-		padding-right: 10px;
 		text-align: center;
 
 		& > span {
--- src/layouts/components/AppSetting/LayoutSelect.vue
+++ src/layouts/components/AppSetting/LayoutSelect.vue
@@ -1,8 +1,77 @@
 <script lang="tsx">
+	import { useLayoutStore } from '@/store';
+	import { unref } from 'vue';
+
 	export default defineComponent({
 		name: 'LayoutSelect',
 		setup() {
-			return <></>;
+			const layoutStore = useLayoutStore();
+			const layoutMode = computed(() => layoutStore.layout);
+
+			const handleChangeLayout = (layout: LayoutConfig) => {
+				layoutStore.$patch({ layout });
+			};
+
+			return () => (
+				<div class="flex-center flex-wrap gap-x-8 gap-y-4">
+					<el-tooltip effect="dark" content="纵向模式" placement="bottom">
+						<div
+							class={[
+								'w-25 h-17 p-1 shadow-[var(--el-box-shadow-lighter)] rounded-1 transition-delay-150 flex gap-x-1 cursor-pointer b-2 b-transparent hover:b-primary',
+								{ 'b-primary': unref(layoutMode) === 'vertical' }
+							]}
+							onClick={() => handleChangeLayout('vertical')}>
+							<div class="w-6 h-full rounded-1 bg-[var(--el-color-primary-light-4)]" />
+							<div class="w-full flex-col gap-y-1">
+								<div class="w-full h-3 bg-[var(--el-color-primary-light-7)] rounded-1" />
+								<div class="w-full flex-1 bg-[var(--el-color-primary-light-9)] rounded-1" />
+							</div>
+						</div>
+					</el-tooltip>
+					<el-tooltip effect="dark" content="经典模式" placement="bottom">
+						<div
+							class="w-25 h-17 p-1 shadow-[var(--el-box-shadow-lighter)] rounded-1 transition-delay-150 flex-col gap-y-1 cursor-pointer b-2 b-transparent hover:b-primary"
+							onClick={() => handleChangeLayout('basic')}>
+							<div class="w-full h-3 bg-[var(--el-color-primary-light-7)] rounded-1" />
+							<div class="w-full flex flex-1 gap-x-1">
+								<div class="w-6 h-full rounded-1 bg-[var(--el-color-primary-light-4)]" />
+								<div class="w-full h-full bg-[var(--el-color-primary-light-9)] rounded-1" />
+							</div>
+						</div>
+					</el-tooltip>
+					<el-tooltip effect="dark" content="横向模式" placement="bottom">
+						<div
+							class="w-25 h-17 p-1 shadow-[var(--el-box-shadow-lighter)] rounded-1 transition-delay-150 flex-col gap-y-1 cursor-pointer b-2 b-transparent hover:b-primary"
+							onClick={() => handleChangeLayout('horizontal')}>
+							<div class="w-full h-3 bg-[var(--el-color-primary-light-4)] rounded-1" />
+							<div class="w-full flex-1 bg-[var(--el-color-primary-light-9)] rounded-1" />
+						</div>
+					</el-tooltip>
+					<el-tooltip effect="dark" content="栅格模式" placement="bottom">
+						<div
+							class="w-25 h-17 p-1 shadow-[var(--el-box-shadow-lighter)] rounded-1 transition-delay-150 flex gap-x-1 cursor-pointer b-2 b-transparent hover:b-primary"
+							onClick={() => handleChangeLayout('lattice')}>
+							<div class="w-2 h-full rounded-1 bg-[var(--el-color-primary-light-4)]" />
+							<div class="w-5 h-full rounded-1 bg-[var(--el-color-primary-light-4)]" />
+							<div class="flex-1 h-full flex-col gap-y-1">
+								<div class="w-full h-3 bg-[var(--el-color-primary-light-7)] rounded-1" />
+								<div class="w-full flex-1 bg-[var(--el-color-primary-light-9)] rounded-1" />
+							</div>
+						</div>
+					</el-tooltip>
+					<el-tooltip effect="dark" content="混合模式" placement="bottom">
+						<div
+							class="w-25 h-17 p-1 shadow-[var(--el-box-shadow-lighter)] rounded-1 transition-delay-150 flex-col gap-y-1 cursor-pointer b-2 b-transparent hover:b-primary"
+							onClick={() => handleChangeLayout('mixins')}>
+							<div class="w-full h-3 bg-[var(--el-color-primary-light-4)] rounded-1" />
+							<div class="w-full flex flex-1 gap-x-1">
+								<div class="w-6 h-full rounded-1 bg-[var(--el-color-primary-light-4)]" />
+								<div class="w-full h-full bg-[var(--el-color-primary-light-9)] rounded-1" />
+							</div>
+						</div>
+					</el-tooltip>
+				</div>
+			);
 		}
 	});
 </script>
