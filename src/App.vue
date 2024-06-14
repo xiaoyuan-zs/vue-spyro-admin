98d0aa799753f3555ff0b051dae8d0b2d3ce2dcd
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-06-15 02:28:43 +0800

    feat: 项目使用pnpm实现本地依赖安装使用

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
 	import zhCN from 'element-plus/es/locale/lang/zh-cn';
 	import en from 'element-plus/es/locale/lang/en';
 	import { initRoutes } from './router/helpers/handleRoutes';
+	import { testAxios } from '@spyro/axios';
 
 	const appStore = useAppStore();
 	const language = computed(() => (appStore.language === 'zh-CN' ? zhCN : en));
 
 	const { initTheme } = useTheme();
 	initTheme();
+
+	const a = testAxios();
+	console.log(121, a);
 
 	initRoutes();
 </script>
