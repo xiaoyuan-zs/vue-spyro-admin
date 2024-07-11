ef38470296a8988ee6c96a110f23ecc372b0dbdb
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-07-11 22:50:41 +0800

    fix: 修改

--- src/views/components/table/treeTable/index.vue
+++ src/views/components/table/treeTable/index.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+	import { buildHierarchyTree } from '@spyro/utils';
 	import { getTrees } from '@/api/tree';
 	import { TreeNode } from '@/api/tree/type';
+
+	getTrees().then((res) => {
+		console.log(655, buildHierarchyTree(res.data));
+	});
 </script>
 <template>
 	<div>treeTable</div>
