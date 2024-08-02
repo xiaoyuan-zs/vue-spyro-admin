bb47f4ebf1c9f63ff7aefdd84ee5eab2c106a41d
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-08-02 16:23:13 +0800

    fix: 完善驾驶舱初始布局

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,8 +2,41 @@
 	defineOptions({
 		name: 'Dashboard'
 	});
+
+	const screen = ref<HTMLDivElement>();
+
+	const initSize = () => {
+		if (screen.value) {
+			screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`;
+			window.addEventListener('resize', resize);
+		}
+	};
+
+	const getScale = (w = 1920, h = 1080) => {
+		const ww = window.innerWidth / w;
+		const wh = window.innerHeight / h;
+		return ww < wh ? ww : wh;
+	};
+
+	const resize = () => {
+		if (screen.value) {
+			screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`;
+		}
+	};
+
+	onMounted(() => {
+		initSize();
+	});
+
+	onBeforeUnmount(() => {
+		window.removeEventListener('resize', resize);
+	});
 </script>
 
 <template>
-	<div class="w-full h-full">23</div>
+	<div class="w-full h-full overflow-hidden">
+		<div ref="screen" class="fixed w-480 h-270 left-2/4 top-2/4 transform-origin-lt">
+			<div class="w-full h-full bg-blue">23</div>
+		</div>
+	</div>
 </template>
--- src/views/home/analysis/index.vue
+++ src/views/home/analysis/index.vue
@@ -3,18 +3,45 @@
 		name: 'Analysis'
 	});
 
-	 const
+	const statistics = [
+		{
+			icon: 'ep:trend-charts',
+			title: '在线评标率',
+			data: '78%',
+			bgColor: 'bg-sky-300'
+		},
+		{
+			icon: 'ant-design:property-safety-filled',
+			title: '进场金额',
+			data: '￥9204.00',
+			bgColor: 'bg-teal-300'
+		},
+		{
+			icon: 'ant-design:account-book-filled',
+			title: '结算金额',
+			data: '￥10273.40',
+			bgColor: 'bg-sky-300'
+		},
+		{
+			icon: 'ant-design:database-filled',
+			title: '开累消耗',
+			data: '6480',
+			bgColor: 'bg-indigo-300'
+		},
+		{
+			icon: 'ant-design:golden-filled',
+			title: '现有库存',
+			data: '￥9204.00',
+			bgColor: 'bg-teal-300'
+		}
+	];
 </script>
 
 <template>
 	<div class="pb-4">
 		<so-card title="数据统计" :underline="false">
 			<div class="w-full grid grid-cols-5 gap-4">
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-				<div>4</div>
-				<div>5</div>
+				<div v-for="(item, index) in statistics" :key="index">{{ item.data }}</div>
 			</div>
 		</so-card>
 	</div>
