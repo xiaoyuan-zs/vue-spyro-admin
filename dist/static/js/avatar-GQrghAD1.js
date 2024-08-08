dcd8cf5a1a41d9dd37e9f427d0795e80078abd93
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-08-08 18:12:08 +0800

    fix: 去除dist

--- dist/static/js/avatar-GQrghAD1.js
+++
@@ -1,1 +0,0 @@
-const a="/static/jpg/avatar-CJgavw0Q.jpg";export{a as _};
--- dist/static/js/useIcon-CCOGsOky.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./Icon.vue_vue_type_script_lang-EAei3ndZ.js";import{ax as s}from"./.pnpm-D59sYBvj.js";const t=o=>s(r,o);export{t as u};
