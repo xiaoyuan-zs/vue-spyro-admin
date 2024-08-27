475269ecd6314e4b7a9691279f1e9952fa05ac59
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-08-27 20:34:30 +0800

    fix: 完善权限自定义指令

--- src/directives/modules/permission.ts
+++ src/directives/modules/permission.ts
@@ -1,5 +1,6 @@
 import type { DirectiveBinding } from 'vue';
 import type { DirectiveOptions } from '../types';
+import { useUserStore } from '@/store';
 
 /**
  * 自定义指令-操作权限处理
@@ -7,7 +8,25 @@ import type { DirectiveOptions } from '../types';
 const hasPerms: DirectiveOptions<'vPermissions'> = {
 	name: 'permissions',
 	directive: {
-		mounted(el: HTMLElement, binding: DirectiveBinding<string[]>) {}
+		mounted(el: HTMLElement, binding: DirectiveBinding<string[]>) {
+			const userStore = useUserStore();
+			const { value } = binding;
+			// 获取用户权限
+			const permissions = userStore.permissions;
+			// 管理员拥有所有权限
+			const allPermission = '*:*:*';
+			if (value.length === 0) {
+				console.error(
+					'v-permissions: value must be an array and not empty! example: hasPermissions(["system:user:add", "system:user:edit"])'
+				);
+				return;
+			}
+			// 判断用户是否有传入的权限
+			const hasPermission = permissions.some((el) => allPermission === el || value.includes(el));
+			if (!hasPermission) {
+				el.parentNode && el.parentNode.removeChild(el);
+			}
+		}
 	}
 };
 
--- src/directives/modules/role.ts
+++ src/directives/modules/role.ts
@@ -1,5 +1,6 @@
 import type { DirectiveBinding } from 'vue';
 import type { DirectiveOptions } from '../types';
+import { useUserStore } from '@/store';
 
 /**
  * 自定义指令-操作权限处理
@@ -8,7 +9,20 @@ const hasRole: DirectiveOptions<'vRoles'> = {
 	name: 'roles',
 	directive: {
 		mounted(el: HTMLElement, binding: DirectiveBinding<string[]>) {
-			console.log(21, binding);
+			const userStore = useUserStore();
+			const { value } = binding;
+			// 获取用户角色
+			const roles = userStore.roles;
+			const allRoles = 'admin';
+			if (value.length === 0) {
+				console.error('v-roles: value must be an array and not empty! example: hasRoles(["admin", "common"])');
+				return;
+			}
+			// 判断用户是否具有某角色
+			const hasRole = roles.some((el) => allRoles === el || value.includes(el));
+			if (!hasRole) {
+				el.parentNode && el.parentNode.removeChild(el);
+			}
 		}
 	}
 };
