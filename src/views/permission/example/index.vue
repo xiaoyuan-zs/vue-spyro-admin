7b07a3af34a388155c4c17fb850550cb31bd8bf8
xiaoyuan-zs-com <lcshuxiaoluo@163.com>
2024-08-27 17:58:25 +0800

    fix: 待完成自定义指令功能

--- src/views/permission/example/index.vue
+++ src/views/permission/example/index.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 	import { useUserStore } from '@/store';
 	import { useRouter } from 'vue-router';
+	import { usePermission } from '@/hooks';
 
 	defineOptions({
-		name: ''
+		name: 'Example'
 	});
 
-	const userStore = useUserStore();
-	const currRole = computed(() => userStore.roles.toString());
-
 	const router = useRouter();
+	const userStore = useUserStore();
+	const { hasPermissions, hasRoles } = usePermission();
+
+	const currRole = computed(() => userStore.roles.toString());
+	const isAdmin = computed(() => hasRoles(['admin']));
+	const isCommon = computed(() => hasRoles(['common']));
+	const isCommonAndNormal = computed(() => hasRoles(['normal', 'common']));
+	const isViewPerm = computed(() => hasPermissions(['system:permissions:view']));
+
+	const permissionObj = {
+		admin: '*:*:*',
+		common: 'system:permissions:view',
+		normal: ''
+	};
 
 	const handleClick = () => router.push({ name: 'PermVisible' });
-
-	const handleChangeRole = (value: string) => {};
+	const handleChangeRole = (value: keyof typeof permissionObj) => {
+		userStore.userInfo.roles = [value];
+		userStore.userInfo.permissions = [permissionObj[value]];
+	};
 </script>
 
 <template>
@@ -39,13 +53,12 @@
 			</div>
 			<div>
 				<h1 class="text-6">函数usePermission</h1>
-				<!-- <el-button-group class="m-4">
-					<el-button v-roles="['admin']">仅admin可见</el-button>
-					<el-button v-roles="['common']">common</el-button>
-					<el-button v-permissions="['system:permissions:view']" @click="handleClick">
-						具有 ['system:permissions:view'] 权限可访问
-					</el-button>
-				</el-button-group> -->
+				<el-button-group class="m-4">
+					<el-button v-if="isAdmin">仅admin可见</el-button>
+					<el-button v-if="isCommon">common可见</el-button>
+					<el-button v-if="isCommonAndNormal">common和normal可见</el-button>
+					<el-button v-if="isViewPerm" @click="handleClick">具有 ['system:permissions:view'] 权限可访问</el-button>
+				</el-button-group>
 			</div>
 		</div>
 	</SoCard>
